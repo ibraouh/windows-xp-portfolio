33c141ab69b83829b9c8712362406d3be2bccb69
Ibrahim Raouh <ibrahim.raouh@gmail.com>
2024-09-09 15:25:00 -0400

    analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./index.css";
 import WindowsXP from "./components/WindowsXP.tsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <div className="App">
       <WindowsXP />
+      <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
